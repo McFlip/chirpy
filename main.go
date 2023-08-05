99fb06843b2b0c56e30eea2a091ebf20de4f8228
McFlip <gradydenton@yahoo.com>
2023-08-04 21:49:59 -0400

    admin

--- main.go
+++ main.go
@@ -29,8 +29,10 @@ func main() {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.Write([]byte("OK"))
 	})
-	apiRouter.Get("/metrics", apiCfg.handlerMetrics)
+	adminRouter := chi.NewRouter()
+	adminRouter.Get("/metrics", apiCfg.handlerMetrics)
 	mainRouter.Mount("/api", apiRouter)
+	mainRouter.Mount("/admin", adminRouter)
 	corsMux := middlewareCors(mainRouter)
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -61,7 +63,8 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+	// w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+	fmt.Fprint(w, fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits))
 }
